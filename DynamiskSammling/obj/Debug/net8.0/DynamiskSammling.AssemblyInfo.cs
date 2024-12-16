890f266b1b5226c827d5e316efd996022e05bb30
samuel <samuel.laestander@edu.lycksele.se>
2024-12-16 13:09:58 +0100

    test10

--- DynamiskSammling/obj/Debug/net8.0/DynamiskSammling.AssemblyInfo.cs
+++ DynamiskSammling/obj/Debug/net8.0/DynamiskSammling.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DynamiskSammling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56eb9a2be65f802308b24bbb6899b4d207db034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f34c59716a773c649d3c5ea73b5ef95c3a9e0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynamiskSammling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynamiskSammling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
